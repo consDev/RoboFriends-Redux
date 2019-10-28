79b67b2fd120b83c3d68d8b6d4bfa6a0f11e12fb
Amirhuusain <meghdadianah@yahoo.com>
2019-10-28 22:06:22 +0330

    Updates

---
+++ precache-manifest.3fa455a7ff52cd2ae7a296b25166afa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efa70799b1825c36f9b98e3463f6f8a",
+    "url": "/RoboFriends-Redux/index.html"
+  },
+  {
+    "revision": "57d113e496a82b6bd60c",
+    "url": "/RoboFriends-Redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2522eaa40f1a94213383",
+    "url": "/RoboFriends-Redux/static/css/main.aa0e77c7.chunk.css"
+  },
+  {
+    "revision": "57d113e496a82b6bd60c",
+    "url": "/RoboFriends-Redux/static/js/2.008a4c71.chunk.js"
+  },
+  {
+    "revision": "2522eaa40f1a94213383",
+    "url": "/RoboFriends-Redux/static/js/main.e54e86bf.chunk.js"
+  },
+  {
+    "revision": "c58bbbde23f98a417525",
+    "url": "/RoboFriends-Redux/static/js/runtime~main.fb39a5bb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends-Redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
