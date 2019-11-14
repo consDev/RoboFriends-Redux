d7a7d929c88d14dc4f4f5a7d421e3100c2a931ce
consDev <amirthedevops@gmail.com>
2019-11-14 12:17:15 +0330

    Changing url

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({id, name , email}) =>
 {
 	return(
 		<div className = 'tc bg-light-green  dib br3 pa3 ma2 bw2 grow shadow-5'>
-			<img src ={`https://robohash.org/${id}`} alt='robots'/>
+			<img src ={`https://www.gravatar.com/avatar/${id}?s=300&d=robohash`} alt='robots'/>
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
